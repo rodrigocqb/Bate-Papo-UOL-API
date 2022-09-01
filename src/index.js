38938109f6e370527f6aea19b56b1a209075e72d
rodrigocqb <rodrigocortibarros@gmail.com>
2022-08-31 22:10:02 -0300

    App base structure with dotenv and mongodb done

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => {
+  db = mongoClient.db("bate_papo_uol");
+});
+
+app.listen(5000, () => console.log("Listening on port 5000!"));
