3be0b3dc79e9cc1d8f6bb5e1a68794fff307d24d
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-01 14:50:08 -0300

    Post request for messages added

--- src/index.js
+++ src/index.js
@@ -16,16 +16,20 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("bate_papo_uol");
 });
 
+async function alreadyExists(participant) {
+  const exists = await db
+    .collection("participants")
+    .findOne({ name: participant });
+  return exists;
+}
+
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
   if (!name) {
     res.sendStatus(422);
     return;
   }
-  const alreadyExists = await db
-    .collection("participants")
-    .findOne({ name: name });
-  if (alreadyExists) {
+  if (alreadyExists(name)) {
     res.sendStatus(409);
     return;
   } else {
@@ -53,6 +57,31 @@ app.get("/participants", async (req, res) => {
   try {
     const participants = await db.collection("participants").find().toArray();
     res.send(participants);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const from = req.headers.user;
+  if (
+    !to ||
+    !text ||
+    !(type === "message" || type === "private_message") ||
+    !from ||
+    !alreadyExists(from)
+  ) {
+    res.sendStatus(422);
+    return;
+  }
+  try {
+    const now = Date.now();
+    await db
+      .collection("messages")
+      .insertOne({ from, to, text, type, time: dayjs(now).format("HH:mm:ss") });
+    res.sendStatus(201);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
